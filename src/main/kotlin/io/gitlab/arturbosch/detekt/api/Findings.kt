50f3aff513f057c7cb1781c4f145bbd0bad7d2ea
brunomateus <brunomateus@gmail.com>
2019-02-14 17:24:17 +0100

    Modification in Deteckt API to include a nem model type of Finding, Feature

--- src/main/kotlin/io/gitlab/arturbosch/detekt/api/Findings.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/api/Findings.kt
@@ -9,13 +9,13 @@ package io.gitlab.arturbosch.detekt.api
  *
  * @author Artur Bosch
  */
-interface Finding : Compactable, HasEntity, HasMetrics {
-    val id: String
-    val issue: Issue
-    val references: List<Entity>
-    val message: String
+sealed class Finding() : Compactable, HasEntity, HasMetrics {
+    abstract val id: String
+    //abstract val issue: Issue
+    abstract val references: List<Entity>
+    abstract val message: String
 
-    fun messageOrDescription(): String
+    open fun messageOrDescription(): String = message
 }
 
 /**
@@ -56,3 +56,59 @@ interface Compactable {
     fun compact(): String
     fun compactWithSignature(): String = compact()
 }
+
+open class Feature(
+    override val id: String,
+    override val references: List<Entity>,
+    override val message: String,
+    override val entity: Entity,
+    override val metrics: List<Metric>
+) : Finding() {
+
+    override fun compact(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+/**
+ * A code smell indicates any possible design problem inside a program's source code.
+ * The type of a code smell is described by an [Issue].
+ *
+ * If the design problem results from metric violations, a list of [Metric]'s
+ * can describe further the kind of metrics.
+ *
+ * If the design problem manifests by different source locations, references to these
+ * locations can be stored in additional [Entity]'s.
+ *
+ * @author Artur Bosch
+ * @author Marvin Ramin
+ */
+open class CodeSmell(
+    private val issue: Issue,
+    override val entity: Entity,
+    override val message: String,
+    override val metrics: List<Metric> = listOf(),
+    override val references: List<Entity> = listOf()) : Finding() {
+
+    override val id: String = issue.id
+
+    override fun compact(): String = "$id - ${entity.compact()}"
+
+    override fun compactWithSignature() = compact() + " - Signature=" + entity.signature
+
+    override fun toString(): String {
+        return "CodeSmell(issue=$issue, " +
+                "entity=$entity, " +
+                "message=$message, " +
+                "metrics=$metrics, " +
+                "references=$references, " +
+                "id='$id')"
+    }
+
+    override fun messageOrDescription() = when {
+        message.isEmpty() -> issue.description
+        else -> message
+    }
+}
+
+
