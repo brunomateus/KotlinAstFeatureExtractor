2ea3958954deb13e7b2795261b9db4347620c602
brunomateus <brunomateus@gmail.com>
2019-02-15 09:19:54 +0100

    Delegation detector

---
+++ src/main/kotlin/fr/uphf/feature/detector/DelegationDetector.kt
@@ -0,0 +1,32 @@
+package fr.uphf.feature.detector
+
+import fr.uphf.analyze.FileAnalyzer
+import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.com.intellij.openapi.util.Key
+import org.jetbrains.kotlin.psi.*
+
+/**
+ * @author Bruno Gois Mateus
+ */
+class DelegationDetector : FileAnalyzer() {
+
+	override fun analyze(file: KtFile): List<Finding> {
+
+		var findings = emptyList<Finding>().toMutableList()
+
+		file.accept(object : KtTreeVisitorVoid() {
+
+			override fun visitDelegatedSuperTypeEntry(specifier: KtDelegatedSuperTypeEntry) {
+				findings.add(Feature(id = "super_delegation", entity = Entity.from(specifier)))
+			}
+
+			override fun visitPropertyDelegate(delegate: KtPropertyDelegate) {
+				findings.add(Feature(id = "property_delegation", entity = Entity.from(delegate)))
+			}
+
+		})
+
+		return findings.toList()
+	}
+
+}
