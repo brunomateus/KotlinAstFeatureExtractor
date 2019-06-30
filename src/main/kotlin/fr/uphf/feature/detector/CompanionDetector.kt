775ec967396bb0ef338d0de09bce506fe52fd850
brunomateus <brunomateus@gmail.com>
2019-06-30 20:42:49 +0200

    Dectector for companion object

---
+++ src/main/kotlin/fr/uphf/feature/detector/CompanionDetector.kt
@@ -0,0 +1,30 @@
+package fr.uphf.feature.detector
+
+import fr.uphf.analyze.FileAnalyzer
+import io.gitlab.arturbosch.detekt.api.*
+import org.jetbrains.kotlin.psi.*
+
+class CompanionDetector :  FileAnalyzer(){
+	override fun analyze(file: KtElement): List<Finding> {
+		var findings = emptyList<Finding>().toMutableList()
+
+		file.accept(object : KtTreeVisitorVoid() {
+
+			override fun visitObjectDeclaration(declaration: KtObjectDeclaration) {
+				super.visitObjectDeclaration(declaration)
+
+
+				if (declaration.isCompanion()){
+					findings.add(
+						Feature(id = "companion",
+							entity = Entity.from(declaration))
+					)
+				}
+			}
+
+		})
+
+		return findings.toList()
+	}
+
+}
